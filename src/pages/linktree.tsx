a240a1b864233527acafb33411758b2b123e9093
Oliver White <whiteoliverg@gmail.com>
2022-08-27 13:51:10 -0400

    head info on linktree page

--- src/pages/linktree.tsx
+++ src/pages/linktree.tsx
@@ -62,3 +62,16 @@ export default function Index() {
     </>
   );
 }
+
+export const Head = () => {
+  return (
+    <>
+      <meta charSet="utf-8" />
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="theme-color" content="#000000" />
+      <meta name="description" content="My resume and projects website" />
+      <title>Oliver White - Resume</title>
+    </>
+  );
+};
